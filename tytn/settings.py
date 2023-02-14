f16dca8b2b6d6f323f8b9ed9a1518a34f3e4efe2
hung222 <90371118+hung222@users.noreply.github.com>
2023-02-14 14:37:03 +0700

    Update settings.py

--- tytn/settings.py
+++ tytn/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = 'RENDER' not in os.environ
 DEBUG = False
 
-ALLOWED_HOSTS = ["*',]
+ALLOWED_HOSTS = ["*",]
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
